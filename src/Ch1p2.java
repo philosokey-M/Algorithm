124cbbb969d6286d86e2891a7b5eee6da175cc32
philosokey-M <wlghd99589@gmail.com>
2024-01-04 21:21:16 +0900

    알고리즘 문제 2번 해결

---
+++ src/Ch1p2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ch1p2 {
+public static void main(String[] args) throws Exception {
+
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("A값 입력 >>");
+    int A = sc.nextInt();
+    
+    System.out.println("B값 입력 >>");
+    int B = sc.nextInt();
+
+
+    System.out.println(A+B); 
+        
+}
+
+}
